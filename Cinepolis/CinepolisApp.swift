ff79a0e725ccb6d6bcc2f00aee287ac97c474cc3
andreassalazar <andrea.sosa01@iest.edu.mx>
2023-10-06 18:18:44 -0600

    Initial Commit

---
+++ Cinepolis/CinepolisApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinepolisApp.swift
+//  Cinepolis
+//
+//  Created by -a.s  on 06/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CinepolisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
